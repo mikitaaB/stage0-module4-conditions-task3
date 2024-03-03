0d5a7522b93342b898ea2e4123f178a9fc2f86e8
Mikita Buinavets <mikita.buinavets@arua.co>
2024-03-04 02:36:36 +0300

    module 4. task 3. fix

--- src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
@@ -2,8 +2,7 @@ package school.mjc.stage0.conditions.task3;
 
 public class IsEnglishSymbolDeterminer {
     public void isEnglishSymbol(char symbol) {
-        if (symbol == 'q' && symbol == 'w' && symbol == 'e' && symbol == 'r' && symbol == 't'
-                && symbol == 'y' && symbol == 'u' && symbol == 'i' && symbol == 'o' && symbol == 'p') {
+        if ( (symbol >= 'a' && symbol <= 'z') || (symbol >= 'A' && symbol <= 'Z') ) {
             System.out.println("English");
         } else {
             System.out.println("Non English");
--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,17 +2,14 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
-        if (salary <= 10000){
-            int taxes = (int) (salary * 0.15);
-            System.out.println(salary - taxes);
-        } else if (10000 < salary || salary <= 20000) {
-            int taxes = (int) (salary * 0.18);
-            System.out.println(salary-taxes);
-        } else if (salary > 20000) {
-            int taxes = (int) (salary * 0.20);
-            System.out.println(salary - taxes);
-        } else if (salary < 0) {
+        if (salary < 0) {
             System.out.println("wrong input!");
+        } else if (salary <= 10_000) {
+            System.out.println(salary * 0.85);
+        } else if (salary <= 20_000) {
+            System.out.println(salary * 0.82);
+        } else {
+            System.out.println(salary * 0.8);
         }
     }
 }
